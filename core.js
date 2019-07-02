3791b05c79384976882b79380fe39a212221572c
Dimasion <mr.dimasion@gmail.com>
2019-07-02 22:55:50 +0300

    Update

---
+++ core.js
@@ -0,0 +1,249 @@
+(function () {
+  // ============================================================
+  // Answers array
+  // ============================================================
+  let answers = []
+
+  var passedCount = 0
+
+  // ============================================================
+  // Main function
+  // ============================================================
+  function testIt (answers) {
+    answers.forEach(item => {
+      var answer = item.answer
+      var search = item.search
+
+      // ============================================================
+      // Search/Answer fields checking
+      // ============================================================
+      if (!search || !answer) return
+
+      if (typeof search !== 'string') return
+      if (item.type === 'checkbox') {
+        if (!(item.answer instanceof Array)) return
+      } else {
+        if (typeof search !== 'string') return
+      }
+
+      // ============================================================
+      // Fill base on item type
+      // ============================================================
+      switch (item.type) {
+        case 'radio':
+          fillRadio(search, answer)
+          break;
+        case 'checkbox':
+          fillCheckbox(search, answer)
+          break;
+        case 'input':
+          fillInput(search, answer)
+          break;
+        case 'select':
+          fillSelect(search, answer)
+          break;
+      }
+    })
+
+    console.log('Success. You passed', passedCount, 'tests.')
+  }
+
+  // ============================================================
+  // Radio
+  // ============================================================
+  function fillRadio (search, answer) {
+    let exeptions = []
+
+    return (function fill() {
+      // QuestionNode
+      var questionNode = findParentNodeByText(search, null, exeptions)
+
+      if (!questionNode) return
+      if (questionNode.nodeName === 'SCRIPT') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      exeptions.push(questionNode)
+
+      // AnswerNode
+      var answerNode = findParentNodeByText(answer, questionNode.parentNode, exeptions)
+
+      if (!answerNode) return
+      if (answerNode.nodeName !== 'LABEL') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      // Result
+      answerNode.click()
+      passedCount = passedCount + 1
+    })()
+  }
+
+  // ============================================================
+  // Checkboxes
+  // ============================================================
+  function fillCheckbox (search, answer) {
+    let exeptions = []
+
+    return (function fill() {
+      // QuestionNode
+      var questionNode = findParentNodeByText(search, null, exeptions)
+
+      if (!questionNode) return
+      if (questionNode.nodeName === 'SCRIPT') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      exeptions.push(questionNode)
+
+      // AnswerNode
+      var firstAnswerNode = findParentNodeByText(answer[0], questionNode.parentNode, exeptions)
+      if (!firstAnswerNode) return
+      if (firstAnswerNode.nodeName !== 'LABEL') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      // Result
+      answer.forEach(item => {
+        var answerNode = findParentNodeByText(item, questionNode.parentNode, exeptions)
+
+        if (!answerNode) return
+        answerNode.click()
+      })
+
+      passedCount = passedCount + 1
+    })()
+  }
+
+  // ============================================================
+  // Inputs
+  // ============================================================
+  function fillInput (search, answer) {
+    let exeptions = []
+
+    return (function fill() {
+      // QuestionNode
+      var questionNode = findParentNodeByText(search, null, exeptions)
+
+      if (!questionNode) return
+      if (questionNode.nodeName === 'SCRIPT') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      exeptions.push(questionNode)
+
+      // AnswerNode
+      var answerNode = questionNode.parentNode.querySelector('input[type="text"]')
+      if (!answerNode) return
+
+      // Result
+      answerNode.value = answer
+      passedCount = passedCount + 1
+    })()
+  }
+
+  // ============================================================
+  // Selects
+  // ============================================================
+  function fillSelect (search, answer) {
+    let exeptions = []
+
+    return (function fill() {
+      // QuestionNode
+      var questionNode = findParentNodeByText(search, null, exeptions)
+
+      if (!questionNode) return
+      if (questionNode.nodeName === 'SCRIPT') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      exeptions.push(questionNode)
+
+      // AnswerNode
+      var answerNode = findParentNodeByText(answer, questionNode.parentNode, exeptions)
+
+      if (!answerNode) return
+      if (answerNode.nodeName !== 'OPTION') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      // Result
+      answerNode.setAttribute('selected', true)
+      passedCount = passedCount + 1
+    })()
+  }
+
+  // ============================================================
+  // Get text node
+  // ============================================================
+  function getTextNodesIn(elem, opt_fnFilter) {
+    var textNodes = [];
+
+    if (elem) {
+      for (var nodes = elem.childNodes, i = nodes.length; i--;) {
+        var node = nodes[i], nodeType = node.nodeType;
+
+        if (nodeType == 3) {
+          if (!opt_fnFilter || opt_fnFilter(node, elem)) {
+            textNodes.push(node);
+          }
+        }
+        else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+          textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
+        }
+      }
+    }
+
+    return textNodes;
+  }
+
+  // ============================================================
+  // Format text
+  // ============================================================
+  function formatText (text) {
+    return text.replace(/\n|\r|\s/g, '').toLowerCase()
+  }
+
+  // ============================================================
+  // Find parent node by text
+  // ============================================================
+  function findParentNodeByText (search, node, exeptNodes) {
+    if (!node) node = document.querySelector('body')
+    if (!exeptNodes) exeptNodes = []
+
+    let parentNode = null
+
+    getTextNodesIn(node, function(textNode, parent) {
+      if (exeptNodes.includes(parent)) return
+
+      var text = formatText(textNode.textContent)
+      var searchText = formatText(search)
+
+      if (!text) return
+      if (text.includes(searchText)) {
+        parentNode = parent
+      }
+    });
+
+    return parentNode
+  }
+
+  // ============================================================
+  // Initialization
+  // ============================================================
+  testIt(answers)
+})()
--- pashkevich/pashkevich_script.js
+++ pashkevich/pashkevich_script.js
@@ -1,4 +1,8 @@
-let answers = [
+(function () {
+  // ============================================================
+  // Answers array
+  // ============================================================
+  let answers = [
     { type: 'radio', search: 'Згідно Agile-маніфесту люди та співпраця важливіші за', answer: 'процеси і інструменти' },
     { type: 'radio', search: 'Згідно Agile-маніфесту працюючий продукт важливіший за', answer: 'повну документацію' },
     { type: 'radio', search: 'Згідно Agile-маніфесту позитивна співпраця із замовником важливіший за', answer: "контрактні" },
@@ -124,7 +128,7 @@ let answers = [
     { type: 'select', search: 'Валідація', answer: 'Забезпечення відповідності' },
     { type: 'select', search: 'Верифікація', answer: 'Перевірка правильності трансформації' },
     { type: 'select', search: 'Агрегація', answer: 'кількох понять у нове поняття' },
-    
+
     { type: 'select', search: 'Каскадна модель', answer: 'в якій кожен етап роботи виконується лише раз' },
     { type: 'select', search: 'Спіральна модель', answer: 'яка дозволяє повертатися до будь-якого попереднього етапу' },
     { type: 'select', search: 'Еволюційна модел', answer: 'в якій система послідовно розробляється з блоків' },
@@ -132,7 +136,7 @@ let answers = [
     { type: 'select', search: 'Rational Unified Process', answer: 'Методологія, що пропонує ітеративну модель розроблення' },
     { type: 'select', search: 'Agile', answer: 'Клас методологій розроблення програмного забезпечення на основі ітеративної' },
     { type: 'select', search: 'eXtreme Programming', answer: 'що приділяє основну увагу ефективній комунікації між замовником' },
-    
+
     { type: 'select', search: 'Люди та співпраця важливіші за', answer: 'процеси та інструменти' },
     { type: 'select', search: 'Працюючий продукт важливіший за', answer: 'вичерпну документацію' },
     { type: 'select', search: 'Позитивна співпраця із замовником важливіша за ', answer: 'обговорення умов контракту' },
@@ -143,7 +147,7 @@ let answers = [
     { type: 'select', search: 'розроблення концепції товару', answer: '3' },
     { type: 'select', search: 'розроблення і створення дослідного зразка', answer: '4' },
     { type: 'select', search: 'пробний маркетинг', answer: '5' },
-    
+
     { type: 'select', search: 'Initiation Project', answer: '1' },
     { type: 'select', search: 'Planning', answer: '2' },
     { type: 'select', search: 'Executive', answer: '3' },
@@ -167,3 +171,247 @@ let answers = [
     { type: 'select', search: 'Інтенсивне використання майже готової версії продукту з метою виявлення', answer: 'Бета-тестування' },
     { type: 'select', search: 'Дослідження, що виконується з метою визначення зручності деякого штучного', answer: 'Юзабіліті-тестування' }
   ]
+
+  var passedCount = 0
+
+  // ============================================================
+  // Main function
+  // ============================================================
+  function testIt (answers) {
+    answers.forEach(item => {
+      var answer = item.answer
+      var search = item.search
+
+      // ============================================================
+      // Search/Answer fields checking
+      // ============================================================
+      if (!search || !answer) return
+
+      if (typeof search !== 'string') return
+      if (item.type === 'checkbox') {
+        if (!(item.answer instanceof Array)) return
+      } else {
+        if (typeof search !== 'string') return
+      }
+
+      // ============================================================
+      // Fill base on item type
+      // ============================================================
+      switch (item.type) {
+        case 'radio':
+          fillRadio(search, answer)
+          break;
+        case 'checkbox':
+          fillCheckbox(search, answer)
+          break;
+        case 'input':
+          fillInput(search, answer)
+          break;
+        case 'select':
+          fillSelect(search, answer)
+          break;
+      }
+    })
+
+    console.log('Success. You passed', passedCount, 'tests.')
+  }
+
+  // ============================================================
+  // Radio
+  // ============================================================
+  function fillRadio (search, answer) {
+    let exeptions = []
+
+    return (function fill() {
+      // QuestionNode
+      var questionNode = findParentNodeByText(search, null, exeptions)
+
+      if (!questionNode) return
+      if (questionNode.nodeName === 'SCRIPT') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      exeptions.push(questionNode)
+
+      // AnswerNode
+      var answerNode = findParentNodeByText(answer, questionNode.parentNode, exeptions)
+
+      if (!answerNode) return
+      if (answerNode.nodeName !== 'LABEL') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      // Result
+      answerNode.click()
+      passedCount = passedCount + 1
+    })()
+  }
+
+  // ============================================================
+  // Checkboxes
+  // ============================================================
+  function fillCheckbox (search, answer) {
+    let exeptions = []
+
+    return (function fill() {
+      // QuestionNode
+      var questionNode = findParentNodeByText(search, null, exeptions)
+
+      if (!questionNode) return
+      if (questionNode.nodeName === 'SCRIPT') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      exeptions.push(questionNode)
+
+      // AnswerNode
+      var firstAnswerNode = findParentNodeByText(answer[0], questionNode.parentNode, exeptions)
+      if (!firstAnswerNode) return
+      if (firstAnswerNode.nodeName !== 'LABEL') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      // Result
+      answer.forEach(item => {
+        var answerNode = findParentNodeByText(item, questionNode.parentNode, exeptions)
+
+        if (!answerNode) return
+        answerNode.click()
+      })
+
+      passedCount = passedCount + 1
+    })()
+  }
+
+  // ============================================================
+  // Inputs
+  // ============================================================
+  function fillInput (search, answer) {
+    let exeptions = []
+
+    return (function fill() {
+      // QuestionNode
+      var questionNode = findParentNodeByText(search, null, exeptions)
+
+      if (!questionNode) return
+      if (questionNode.nodeName === 'SCRIPT') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      exeptions.push(questionNode)
+
+      // AnswerNode
+      var answerNode = questionNode.parentNode.querySelector('input[type="text"]')
+      if (!answerNode) return
+
+      // Result
+      answerNode.value = answer
+      passedCount = passedCount + 1
+    })()
+  }
+
+  // ============================================================
+  // Selects
+  // ============================================================
+  function fillSelect (search, answer) {
+    let exeptions = []
+
+    return (function fill() {
+      // QuestionNode
+      var questionNode = findParentNodeByText(search, null, exeptions)
+
+      if (!questionNode) return
+      if (questionNode.nodeName === 'SCRIPT') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      exeptions.push(questionNode)
+
+      // AnswerNode
+      var answerNode = findParentNodeByText(answer, questionNode.parentNode, exeptions)
+
+      if (!answerNode) return
+      if (answerNode.nodeName !== 'OPTION') {
+        exeptions.push(questionNode)
+        fill(search, answer)
+        return
+      }
+
+      // Result
+      answerNode.setAttribute('selected', true)
+      passedCount = passedCount + 1
+    })()
+  }
+
+  // ============================================================
+  // Get text node
+  // ============================================================
+  function getTextNodesIn(elem, opt_fnFilter) {
+    var textNodes = [];
+
+    if (elem) {
+      for (var nodes = elem.childNodes, i = nodes.length; i--;) {
+        var node = nodes[i], nodeType = node.nodeType;
+
+        if (nodeType == 3) {
+          if (!opt_fnFilter || opt_fnFilter(node, elem)) {
+            textNodes.push(node);
+          }
+        }
+        else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+          textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
+        }
+      }
+    }
+
+    return textNodes;
+  }
+
+  // ============================================================
+  // Format text
+  // ============================================================
+  function formatText (text) {
+    return text.replace(/\n|\r|\s/g, '').toLowerCase()
+  }
+
+  // ============================================================
+  // Find parent node by text
+  // ============================================================
+  function findParentNodeByText (search, node, exeptNodes) {
+    if (!node) node = document.querySelector('body')
+    if (!exeptNodes) exeptNodes = []
+
+    let parentNode = null
+
+    getTextNodesIn(node, function(textNode, parent) {
+      if (exeptNodes.includes(parent)) return
+
+      var text = formatText(textNode.textContent)
+      var searchText = formatText(search)
+
+      if (!text) return
+      if (text.includes(searchText)) {
+        parentNode = parent
+      }
+    });
+
+    return parentNode
+  }
+
+  // ============================================================
+  // Initialization
+  // ============================================================
+  testIt(answers)
+})()
